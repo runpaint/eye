aae0940a8108713af0b54634aa3b627d6aec6600
Run Paint Run Run <runrun@runpaint.org>
2009-04-27 02:51:56 +0100

    Made .see? spec use the new `eyes` helper method.

--- spec/eye/see_spec.rb
+++ spec/eye/see_spec.rb
@@ -1,29 +1,36 @@
 describe "Eye.see" do
-
-  before(:each) do
-    @eye = Eye.new
-  end
+  include HelperMethods
 
   it "is a method" do
-    @eye.should.respond_to? :see
+    eyes.each do |eye|
+      eye.should.respond_to? :see
+    end  
   end
  
   it "takes a single argument" do
-    lambda{@eye.see("string")}.should_not raise_error
+    eyes.each do |eye|
+      lambda{eye.see("string")}.should_not raise_error
+    end  
   end
 
   it "raises an ArgumentError if called without arguments" do
-    lambda{@eye.see}.should raise_error(ArgumentError)
+    eyes.each do |eye|
+      lambda{eye.see}.should raise_error(ArgumentError)
+    end  
   end
   
   it "raises an ArgumentError if called with more than one argument" do
-    lambda{@eye.see(1,2)}.should raise_error(ArgumentError)
-    lambda{@eye.see("string",['glark'],"bar")}.should raise_error(ArgumentError)
+    eyes.each do |eye|
+      lambda{eye.see(1,2)}.should raise_error(ArgumentError)
+      lambda{eye.see("string",['glark'],"bar")}.should raise_error(ArgumentError)
+    end
   end
   
   it "returns true if the object was recorded" do
-    ['Kim', 'http://example.com/'].each do |obj|
-      @eye.see(obj).should == true
+    eyes.each do |eye|
+      ['Kim', 'http://example.com/'].each do |obj|
+        eye.see(obj).should == true
+      end
     end  
   end
 
